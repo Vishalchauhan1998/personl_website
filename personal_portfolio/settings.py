c735c8f35e073ab8d3e72ba06ece1f27d511b45d
Vishal Chauhan <49778609+Vishalchauhan1998@users.noreply.github.com>
2022-09-25 21:52:44 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'personal_portfolio.urls'
 
 TEMPLATES = [
