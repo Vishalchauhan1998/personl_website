631fa4ba9c03b45e79f549ef761e63d77649f260
Vishal Chauhan <49778609+Vishalchauhan1998@users.noreply.github.com>
2022-09-25 21:49:30 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # static files
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
